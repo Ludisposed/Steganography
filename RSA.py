45361a032dd7b87961bb2f810e87188006ea6d2d
Ludisposed <pjgvanschaik@gmail.com>
2017-07-13 10:31:16 +0200

    RSA test

--- RSA.py
+++ RSA.py
@@ -1,3 +1,51 @@
-k = format(134, 'b')
-print k
-print int(k[::-1][-1]) & 1
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives.asymmetric import rsa
+from cryptography.hazmat.primitives import serialization
+
+
+# RSA key generation
+private_key = rsa.generate_private_key(
+     public_exponent=65537,
+     key_size=2048,
+     backend=default_backend()
+)
+
+# RSA key loading
+with open("path/to/key.pem", "rb") as key_file:
+    private_key = serialization.load_pem_private_key(
+        key_file.read(),
+        password=None,
+        backend=default_backend()
+    )
+
+# Key serialization
+pem = private_key.private_bytes(
+   encoding=serialization.Encoding.PEM,
+   format=serialization.PrivateFormat.PKCS8,
+   encryption_algorithm=serialization.BestAvailableEncryption(b'mypassword')
+)
+pem.splitlines()
+
+# RSA encryption using public key
+message = b"encrypted data"
+ciphertext = public_key.encrypt(
+    message,
+    padding.OAEP(
+        mgf=padding.MGF1(algorithm=hashes.SHA1()),
+        algorithm=hashes.SHA1(),
+        label=None
+    )
+)
+
+# RSA decryption using private key
+plaintext = private_key.decrypt(
+    ciphertext,
+    padding.OAEP(
+        mgf=padding.MGF1(algorithm=hashes.SHA1()),
+        algorithm=hashes.SHA1(),
+        label=None
+    )
+)
+plaintext == message
+
+
