5da2fd67fe4df37f6b3bea7b5bf544400a5aeb42
Qin <aries.duanmu@gmail.com>
2017-07-14 22:24:00 +0800

    tester

--- prng_stego/tester_mac.sh
+++ prng_stego/tester_mac.sh
@@ -1,10 +1,11 @@
+# chmod a+x ./tester_mac.sh
 echo "I was tired of typing everything in all the time. xD"
 echo "## prng_stego tester [bash] ##"
 echo ""
 echo "TEST 1 -- normal from file"
 echo "Encrypt test:"
 echo "python prng_stego.py -e -p pass -m magic test.png file_test.txt"
-stdbuf -oL python prng_stego.py -e -p pass -m magic test.png file_test.txt |
+python2.7 prng_stego.py -e -p pass -m magic test.png file_test.txt |
     while IFS= read -r line
     do
       echo "$line"
@@ -12,7 +13,7 @@ stdbuf -oL python prng_stego.py -e -p pass -m magic test.png file_test.txt |
 echo ""
 echo "Decrypt test:"
 echo "python prng_stego.py -d -p pass -m magic new_test.png"
-stdbuf -oL python prng_stego.py -d -p pass -m magic new_test.png |
+python2.7 prng_stego.py -d -p pass -m magic new_test.png |
     while IFS= read -r line
     do
       echo "$line"
@@ -21,16 +22,16 @@ echo ""
 
 echo "TEST 2 -- fail file"
 echo "Encrypt test:"
-echo "python prng_stego.py -e -p somelongasspassword12345 -m somelongasspassword12345 test.png file_test.txt.incorrect"
-stdbuf -oL python prng_stego.py -e -p somelongasspassword12345 -m somelongassmagic54321 test.png file_test.txt.incorrect |
+echo "python prng_stego.py -e -p somelongasspassword12345 -m somelongassmagic54321 test.png file_test.txt.incorrect"
+python2.7 prng_stego.py -e -p somelongasspassword12345 -m somelongassmagic54321 test.png file_test.txt.incorrect |
     while IFS= read -r line
     do
       echo "$line"
     done
 echo ""
 echo "Decrypt test:"
-echo "python prng_stego.py -d -p somelongasspassword12345 -m somelongasspassword12345 new_test.png"
-stdbuf -oL python prng_stego.py -d -p somelongasspassword12345 -m somelongassmagic54321 new_test.png |
+echo "python prng_stego.py -d -p somelongasspassword12345 -m somelongassmagic54321 new_test.png"
+python2.7 prng_stego.py -d -p somelongasspassword12345 -m somelongassmagic54321 new_test.png |
     while IFS= read -r line
     do
       echo "$line"
@@ -40,7 +41,7 @@ echo ""
 echo "TEST 3 -- Long file input"
 echo "Encrypt test:"
 echo "python prng_stego.py -e -p ewvdd3830sd12A -m A21ds0383ddvwe test.png tester.sh"
-stdbuf -oL python prng_stego.py -e -p ewvdd3830sd12A -m A21ds0383ddvwe test.png tester.sh |
+python2.7 prng_stego.py -e -p ewvdd3830sd12A -m A21ds0383ddvwe test.png tester.sh |
     while IFS= read -r line
     do
       echo "$line"
@@ -48,7 +49,7 @@ stdbuf -oL python prng_stego.py -e -p ewvdd3830sd12A -m A21ds0383ddvwe test.png 
 echo ""
 echo "Decrypt test:"
 echo "python prng_stego.py -d -p ewvdd3830sd12A -m A21ds0383ddvwe new_test.png"
-stdbuf -oL python prng_stego.py -d -p ewvdd3830sd12A -m A21ds0383ddvwe new_test.png |
+python2.7 prng_stego.py -d -p ewvdd3830sd12A -m A21ds0383ddvwe new_test.png |
     while IFS= read -r line
     do
       echo "$line"
